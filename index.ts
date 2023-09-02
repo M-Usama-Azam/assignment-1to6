e122a4537ea71c776d2060b017cddfc0c76d0378
M-Usama-Aam <muhammadusama122345@gmail.com>
2023-09-02 12:53:06 +0500

    assignment1to6

--- index.ts
+++ index.ts
@@ -238,24 +238,407 @@ console.log(calculates);
 // 10 - Write a program that takes the number of units consumed by a user if it is greater than 100 then add 10% tax if greater
 // than 200 then add 15% of tax so on up to if greater than 500 then add 25% of tax
 // Where the tax amount will be calculated by the amount of bill.
-var units:number = 149
-var costOfUnit:number = 43
-if(units>100&&units<200){
- units=units*10/100+units
- console.log(units);
-   }
-else if(units>200&&units<300){
- units=units*15/100+units
- console.log(units);
-}else if(units>300&&units<500){
- units=units*20/100+units
- console.log(units);
-}else if(units>500){
- units=units*25/100+units
- console.log(units);
-}else{
- console.log("valid input");
-}
+     var units:number = 149
+    var costOfUnit:number = 43
+    if(units>100&&units<200){
+        units=units*10/100+units
+        console.log(units);
+    }
+    else if(units>200&&units<300){
+        units=units*15/100+units
+        console.log(units);
+    }else if(units>300&&units<500){
+        units=units*20/100+units
+        console.log(units);
+    }else if(units>500){
+        units=units*25/100+units
+        console.log(units);
+    }else{
+        console.log("valid input");
+    }
 var bill : number = units * costOfUnit
 console.log(bill);
  
+
+
+
+
+
+
+
+// Assignment 4
+
+
+
+
+
+
+
+// - Create a function that takes an array, an index, and a value as parameters. Inside the function,
+// use the splice method to insert the value at the specified index in the array. Return the modified array.
+
+
+
+
+function modify(arry: number[], index: number, value: number) {
+    arry.splice(index, 0, value)
+    return arry
+}
+let arra: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let valu: number = 17
+let indx: number = 4
+console.log(modify(arra, indx, valu));
+
+
+// 2  - Implement a simple shopping cart program using an array. Create functions to add items, remove items, and update quantities using the splice method. Print the cart's contents after each operation
+var shopingcart: string[] = ["books", "stationary", "bag", "watch", "uniform"]
+function modifycart(arys: string[], indexe: number, value: string) {
+    arys.splice(indexe, 3, value)
+    // console.log(arys);
+    return arys
+
+}
+var val: string = "choclate"
+var indexe: number = 2
+console.log(modifycart(shopingcart, indexe, val));
+
+
+// 3 - Write a program that uses a while loop to print the first 25 integers.
+
+
+
+var n1:number=1
+ while(n1<=25){
+ console.log(n1);
+ n1++
+ }
+
+
+// 4 - Write a program that uses a while loop to print the first 10 even numbers.
+
+ 
+var n2: number = 1
+ while (n2 <= 20) {
+     if (n2 % 2 == 0) {
+         console.log(n2);
+             }
+                  n2++
+                }
+
+
+//5- Create a function that takes a positive integer as parameter and uses a while loop to calculate and return the factorial of that number.
+
+function fact(num1: number) {
+    var num2: number = num1 - 1
+    if (num1 > 0) {
+        while (num1 > 0) {
+            num2 = num2 * num1
+            num1--
+        }
+        console.log(num2);
+    }
+    return num1
+}
+var factoria:number|undefined=fact(5)
+console.log(factoria);
+
+
+// 6 - Write a program having an array of numbers if the number is negative it should remove the negative number from the array.
+
+function removeNegatives(array: number[]) {
+    // Create a new array to store the positive numbers.
+    const positiveNumbers = [];
+    // Iterate over the original array.
+    for (const number of array) {
+        // If the number is positive, add it to the new array.
+        if (number >= 0) {
+            positiveNumbers.push(number);
+        }
+    }
+    // Return the new array.
+    return positiveNumbers;
+}
+
+const aray = [1, -2, 3, -4, 5, 7, 23, -1234, 50];
+const positiveNumbers = removeNegatives(aray);
+console.log(positiveNumbers); // [1, 3, 5]
+
+
+
+
+// 7 - Create a function that takes an array of numbers as parameter. Use a while loop to calculate and return the sum of all the numbers in the array.
+var numbAry:number[]=[1,2,3,6,7,8,9]
+function numbArry(num8:number[]) {
+    var ind:number=0
+    var sums:number=0
+    while (ind<num8.length) {
+        sums=sums+num8[ind]
+        ind++
+    }
+    return sums
+}
+console.log(numbArry(numbAry));
+
+
+
+// 8 - Implement a program that takes a list of temperatures in Celsius as input from the user. Convert each temperature to Fahrenheit using the formula F = (C * 9/5) + 32 and store the converted temperatures in an array. Use a while loop to perform the conversion for each temperature.
+
+
+
+
+var tempinc1:number[]=[9,20,1,17]
+var tempinf1:number=tempinc1[0]*9/5+32
+console.log(tempinf1)
+var tempinf2:number=tempinc1[1]*9/5+32
+console.log(tempinf2);
+var tempinf3:number=tempinc1[2]*9/5+32
+console.log(tempinf3);
+var tempinf4:number=tempinc1[3]*9/5+32
+console.log(tempinf4);
+var tempinf5:number=tempinc1[3]*9/5+32
+console.log(tempinf5);
+
+
+
+
+
+
+
+
+
+
+// Assignment 5
+
+
+
+
+
+
+
+
+// Write the program that uses filter to remove all negative numbers from an array of number
+
+    let arr2:number[]=[2,4,-5,-6,12,-1]
+    console.log("Original Array",arr2);  
+    let arriUpdated1=arr2.filter((data)=>{
+    
+        if (data>0){
+            return data;
+        }
+    });
+    console.log("Updated Data",arriUpdated1);
+
+
+
+
+
+
+
+// Given an array of string [apple,banana,cheery, date, grape, ] use the filter method to create the new array containing only the fruits with more than 5 characters?
+    
+
+
+    let FruitArray:string[]=["apple","banana","cheery","date","grape"];
+    console.log('Fruits',FruitArray);
+    let NewFruitArray=FruitArray.filter((arr)=>{
+        return arr.length>5;
+    });
+    console.log('Updated Fruits',NewFruitArray);
+
+
+
+
+// Given of array of number[1,2,3,4,5], use the map method to create a new array where each number is multiply by 2?
+
+
+
+    let arr1:number[]=[1,2,3,4,5]
+    console.log("Original Data ",arr1);  //original array
+    let arriUpdated=arr1.map((data)=>{
+        return data*2;
+    });
+    console.log("Updated Data",arriUpdated);
+
+
+
+
+// Given an array of numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use the map and filter methods together to create a new array containing the squares of even numbers?
+
+
+
+    let arrSaqure:number[]=[1,2,3,4,5,6,7,8,9,10]
+    console.log('Original Value of Array ',arrSaqure);
+    let SaqureEvenNumber=arrSaqure.filter((evenNum)=>{
+        return evenNum%2==0;
+
+    }).map((evenNum)=>{
+        return evenNum*evenNum;
+    
+    });
+    console.log('Saqure of evan number',SaqureEvenNumber)
+
+
+
+
+
+// Given an array of temperatures in Celsius [0, 10, 20, 30, 40], use the map method to create a new array where each temperature is converted to Fahrenheit using the formula (Celsius * 9/5) + 32.?
+
+
+
+
+    let tempOfCalsius:number[]=[0,10,20,30,40];
+    console.log('Temperature of Celsius',tempOfCalsius);
+    let tempToFahrnheit=tempOfCalsius.map((Celsius)=>{
+        return (Celsius*9/5)+32
+
+    }); 
+    console.log('Celsius to Fahrnheit',tempToFahrnheit);
+
+
+
+
+// Given an array of numbers [3, 6, 9, 12, 15, 18], use the map and filter methods together to create a new array containing the doubled values of odd numbers in typescript?
+
+
+
+
+    let OldNumberValue:number[]=[3,6,9,12,15,18]
+    console.log('Original Value of array',OldNumberValue)
+    let DoubleOddNumber=OldNumberValue.filter((value)=>{
+        return value % 2 !==0;
+    }).map((OddValue)=>{
+        return OddValue*2;
+    });
+    console.log('Double Odd Numbers',DoubleOddNumber);
+
+
+
+
+
+// Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an exclamation mark at the end, e.g., "Alice!"?
+
+
+
+
+    let UserName:String[]=["Alice","Bob","Charlie","David","Emily"];
+    console.log('Original Name',UserName);
+    UserName.forEach((name)=>{
+        console.log(name+'!');
+    });
+
+
+
+
+
+
+
+
+
+
+    // Assignment 6
+
+
+
+
+
+
+// - Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
+    
+
+
+function sumOfEvenNumber(n: number): number {
+    let sum = 0
+    for (let i = 0; i <= n; i++) {
+        if ((i % 2) == 0) {
+            sum += i
+        }
+    }
+    return sum
+    }
+    console.log(sumOfEvenNumber(10));
+
+
+
+    //  - Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
+
+
+
+    let arrayOfNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for (let index = 0; index < arrayOfNumbers.length; index++) {
+    const element = arrayOfNumbers[index];
+        if (element % 2 == 0) {
+            console.log(element);
+        }
+    }
+
+
+
+
+    // - Implement a program that uses a loop to iterate through an array of numbers and remove all the even numbers from them and just leave the odd ones
+
+
+
+
+    let aryOfNumbers:number[]=[1,2,3,4,53,34,756,324,21]
+    for (let index = 0; index < aryOfNumbers.length; index++) {
+        const element = aryOfNumbers[index];
+            if (element%2==1) {
+                    console.log(element);
+            }
+    }
+
+
+
+
+    // - Write a program that defines a function to calculate the area of a circle. The function should take the radius as input and return the calculated area.
+    
+    
+    
+    
+    function areaOfCircle(radius:number):number {
+        let formula:number= 3.14*(radius**2)
+        return formula
+    }
+    console.log("area of circle is aproximately ",areaOfCircle(7));
+
+
+
+// - Develop a program that reads a list of grades and uses the splice method to remove failing grades (below 50) from the array.
+
+
+
+
+var Passinggrades = (gradesarry:number[]):number[] => {
+    for (let index =gradesarry.length-1;index>=0; index--) {
+        if (gradesarry[index]<50) {
+        gradesarry.splice(index,1)
+
+        }
+    }      
+      return gradesarry
+
+}
+var Passinggrades1 = Passinggrades([43,67,58,97,19,52,34,56,34,29,13,44,23,42,8,66,23,74,39]);
+console.log(Passinggrades1);
+
+
+
+
+// - Write a program that uses a function to find the largest element in an array of numbers.
+
+
+
+function findnumb(arry:number[]) {
+    let largest=arry[0]
+    for (let i=0;i<=arry.length;i++){
+       if(arry[i]>largest){
+       largest=arry[i]}
+    }
+    return largest
+ }
+ var findnumb1=findnumb([1,2,3,4,5,6,7,8,9,14,64,227,24,7,97]);
+ console.log(findnumb1);
+
+
+
+
